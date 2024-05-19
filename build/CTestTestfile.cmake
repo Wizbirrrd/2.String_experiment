06fb985661d96bc14901797cacf66062a31beb18
WuHongbin <14360210+wizbirrrd@user.noreply.gitee.com>
2024-05-19 21:24:44 +0800

    第一次提交

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wizbird/Code/String_experiment
+# Build directory: /home/wizbird/Code/String_experiment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
